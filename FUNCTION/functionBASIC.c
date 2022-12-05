afb393a685864294a8e61066cc68e89fddc2f83d
Shan Shuvo <rtsshuvo6434@gmail.com>
2022-12-06 00:08:38 +0600

    initial

--- FUNCTION/functionBASIC.c
+++ FUNCTION/functionBASIC.c
@@ -1,8 +1,32 @@
 #include<stdio.h>
+void div()
+{
+    int a,b,div=1;
+    printf("\nEnter two numbers for division: ");
+    scanf("%d %d",&a, &b);
+    div = a/b;
+    printf("\nDivision: %d",div);
+}
+void mul()
+{
+    int a,b,mul=1;
+    printf("\nEnter two numbers for multiplication: ");
+    scanf("%d %d",&a, &b);
+    mul = a*b;
+    printf("\nMultiplication: %d",mul);
+}
+void sub()
+{
+    int a,b,sub=0;
+    printf("\nEnter two number for substraction: ");
+    scanf("%d %d",&a,&b);
+    sub = a-b;
+    printf("\nSub: %d", sub);
+}
 void sum()
 {
     int a,b,sum=0;
-    printf("\nEnter two integer: ");
+    printf("\nEnter two integer for summation: ");
     scanf("%d %d", &a, &b);
     sum = a+b;
     printf("\nSum: %d", sum);
@@ -10,8 +34,12 @@ void sum()
 void main()
 {
     sum();
-    printf("\n2nd Time");
+    sub();
+    mul();
+    div();
+    printf("\nAGAIN DO THIS CALCULATION: ");
     sum();
-    printf("\n3rd Time");
-    sum();
+    sub();
+    mul();
+    div();
 }
